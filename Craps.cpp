df05e124fc580b7a4e9b6fb3c25db1f01bedefd4
HamitBerkCeyhan <109693570+HamitBerkCeyhan@users.noreply.github.com>
2022-07-20 22:17:19 +0300

    Update Craps.cpp

--- Craps.cpp
+++ Craps.cpp
@@ -3,38 +3,43 @@
 #include <stdlib.h>
 
 int main(){
-	int a,b,toplam,puan,kilit=0;
+	int a,b,toplam,puan;
 	srand(time(NULL));
-	a=rand()%6+1;
-    b=rand()%6+1;
-    toplam=a+b;
-    printf("Zarlar toplami: %d\n",toplam);
+	a = rand() % 6 + 1;
+    	b = rand() % 6 + 1 ;
+    	toplam=a+b;
+    	printf("Zarlar toplami: %d\n",toplam);
     
-    if(toplam==7 || toplam==11){
-    	printf("Tebrikler Kazandiniz...");
+    	if(toplam == 7 || toplam == 11)
+	{
+    		printf("Tebrikler Kazandiniz...");
 	}
-	if(toplam==2 || toplam==3 || toplam==12){
+	if(toplam == 2 || toplam == 3 || toplam == 12)
+	{
 		printf("Kaybettiniz...");
 	}
 	else{
 		puan=toplam;
-	    printf("Puaniniz: %d\n", puan);
-	    while(1){
-	    printf("Yeni zar atmak icin ENTER tusuna basiniz.");
-	    getchar();
-	a=rand()%6+1;
-    b=rand()%6+1;
-    toplam=a+b;
-    printf("toplam: %d\n",toplam);
-    if(toplam==7){
-    	printf("Kaybettiniz...");
-    	break;
-    }
-	if(toplam==puan){
-    	printf("Tebrikler Kazandiniz...");
-    	break;
-	   }
+		printf("Puaniniz: %d\n", puan);
+		while(1)
+		{
+	    		printf("Yeni zar atmak icin ENTER tusuna basiniz.");
+	    		getchar();
+			a = rand() % 6 + 1;
+    			b = rand() % 6 + 1;
+    			toplam=a+b;
+    			printf("toplam: %d\n",toplam);
+    			if(toplam == 7)
+			{
+    				printf("Kaybettiniz...");
+    				break;
+    			}
+			if(toplam == puan)
+			{
+    				printf("Tebrikler Kazandiniz...");
+    				break;
+	   		}
+		}
 	}
-}
 }
 
